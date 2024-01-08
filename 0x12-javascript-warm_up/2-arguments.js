30698089d50dc15b0e196fb71188956cdab0d2fc
SireCourage001 <sirecourage@gmail.com>
2024-01-08 13:46:41 +0000

    edited_js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 2) {
-console.log("No argument");
-} else if (args.length == 3); {
-console.log("Argument found");
-} else {
-console.log("Arguments found");
+const args = process.argv.length;
+if (args === 2) {
+	console.log("No argument");
+} else if (args > 2); {
+	console.log("Argument found");
 }
