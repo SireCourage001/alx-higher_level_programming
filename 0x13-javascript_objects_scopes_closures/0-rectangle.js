3d86c86d6fefadf8c1462feed6d36e8b0c155b61
SireCourage001 <sirecourage@gmail.com>
2024-01-09 13:05:56 +0000

    js_objects_scopes

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+class Rectangle {}
+modlue.export = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//Use of constructor in taking 2 arguments
+class Rectangle {
+	constructor (w, h) {
+	this.width = w;
+	this.height = h;
+	}
+}
+module.export = Rectangle;
+
